b8738ddf00f381cd7f910a807d0ff58cbf71984f
lvovandrey <lvovandrey@mail.ru>
2019-04-25 00:08:05 +0300

    Готово

---
+++ ChangeWallpaper/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeWallpaper
+{
+    public partial class Form1 : Form
+    {
+        int timetoend = 8;
+        Timer t;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            label4.Text = timetoend.ToString();
+            t = new Timer();
+            t.Enabled = true;
+            t.Interval = 1000;
+            t.Tick += T_Tick;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            timetoend--;
+            label4.Text = timetoend.ToString();
+            if (timetoend < 0) this.Close(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Enabled = false;
+            label4.Text = "";
+            label3.Text = "";
+        }
+    }
+}
--- ChangeWallpaper/Program.cs
+++ ChangeWallpaper/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ChangeWallpaper
 {
     class Program
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, IntPtr lpvParam, int fuWinIni);
+
+        public const int SPI_SETDESKWALLPAPER = 20;
+        public const int SPIF_UPDATEINIFILE = 0x1;
+        public const int SPIF_SENDWININICHANGE = 0x2;
         static void Main(string[] args)
         {
+            try
+            {
+
+                int filescount = new DirectoryInfo(Environment.CurrentDirectory + @"\wallpapers").GetFiles("*.jpg").Length;
+                Random rand = new Random();
+                string filepath = Directory.GetFiles(Environment.CurrentDirectory + @"\wallpapers")[rand.Next(filescount)];
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 1, Marshal.StringToBSTR(filepath), SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+            }
+            catch
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
